a0c46b0fc9a12d1460c4056894ba6993e064ab49
javedahmedone <javedahmedone@gmail.com>
2025-08-10 13:34:15 +0530

    cqrs update

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from routes import auth, portfolio, promtAnalyzer, stock_setup
 
 app = FastAPI()
 
 # Allow frontend CORS
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # for development, restrict in prod
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for development, restrict in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 print("main py")
